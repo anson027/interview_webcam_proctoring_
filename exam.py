6bcfabec44b6ff413d93e28f0ebd6d888e397792
anson <ansonathomas777@gmail.com>
2025-07-22 12:18:57 +0530

    updated

--- exam.py
+++ exam.py
@@ -10,17 +10,20 @@ faces=mpfaces.FaceDetection()
 video=cv2.VideoCapture(0)
 n=10
 correct_position=None
+
+threshold=45 # for eye ear distance
 while True:
   suc,img=video.read()
   img1=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
   result=faces.process(img1)
   lx,ly,rx,ry=245,150,490,400
   h,w,_=img.shape
-  cv2.rectangle(img,(lx,ly),(rx,ry),(0,0,255),3)
 
   c=(0,0,255)
-  sentence='NO face detected'
-  if result.detections:
+  if not result.detections:
+    c=(0,0,255)
+    sentence='No face detected'
+  else:
     for detection in result.detections:
       right_eye=detection.location_data.relative_keypoints[0]
       left_eye=detection.location_data.relative_keypoints[1]
@@ -28,6 +31,19 @@ while True:
       mouth_center=detection.location_data.relative_keypoints[3]
       right_ear=detection.location_data.relative_keypoints[4]
       left_ear=detection.location_data.relative_keypoints[5]
+
+
+      r_ear_x=right_ear.x
+      r_ear_y=right_ear.y
+      r_eye_x=right_eye.x
+      r_eye_y=right_eye.y
+      l_ear_x=left_ear.x
+      l_ear_y=left_ear.y
+      l_eye_x=left_eye.x
+      l_eye_y=left_eye.y
+      left_eye_ear_dist=((((l_eye_x-l_ear_x)**2)*w*w) + (((l_eye_y-l_ear_y)**2)*h*h))**(0.5)
+      right_eye_ear_dist=((((r_eye_x-r_ear_x)**2)*w*w) + (((r_eye_y-r_ear_y)**2)*h*h))**(0.5)
+
       if right_ear.x > right_eye.x or left_ear.x < left_eye.x or right_eye.y>=right_ear.y:
         correct_position=False
         if right_ear.x > right_eye.x:
@@ -37,27 +53,41 @@ while True:
         if right_eye.y>=right_ear.y:
           print("Head turns downwards")
         c=(0,0,255)
-        winsound.Beep(700,100)
+        winsound.Beep(1000,500)
         sentence="Unauthorized activity detected."
       else:
         c=(0,255,0)
         sentence=f'{n} chances to continue the exam'
+        if left_eye_ear_dist<threshold or right_eye_ear_dist<threshold:
+          c=(0,0,255)
+          sentence='Eye is not aligned straight'
+          
+
+      
       if int(right_ear.x*w)<lx or int(left_ear.x*w)>rx or int(mouth_center.y*h)>ry or int(left_eye.y*h)<ly or int(right_eye.y*h)<ly:
         c=(0,0,255)
         sentence="Face not aligned properly"
+        if right_ear.x > right_eye.x or left_ear.x < left_eye.x or right_eye.y>=right_ear.y or left_eye_ear_dist<threshold or right_eye_ear_dist<threshold:
+          sentence="Unauthorized activity detected."
+
+      
       if correct_position==False and right_ear.x < right_eye.x and left_ear.x > left_eye.x and right_eye.y < right_ear.y:
         correct_position=True
         n-=1
+
+      
       if n==0:
-        c=(0,0,255)
+        c=(255,255,255)
         sentence="Distraction Limit Exceeded"
-      cv2.rectangle(img,(lx,ly),(rx,ry),c,3)
-      cv2.putText(img,sentence,(30,30),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.9,color=c,thickness=3)
-      if n==0:  
-        cv2.imshow('EXAM',img)
-        cv2.waitKey(5000)
-        pg.press('q')
-        break
+      mpdrawings.draw_detection(img,detection)
+    cv2.rectangle(img,(lx,ly),(rx,ry),c,3)
+    cv2.putText(img,sentence,(30,30),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.9,color=c,thickness=3)
+    if n==0:
+      img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+      cv2.imshow('EXAM',img)
+      cv2.waitKey(4500)
+      pg.press('q')
+      break
   cv2.imshow('EXAM',img)
   if cv2.waitKey(1) & 0XFF==ord('q'):
     break
